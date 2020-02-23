abf55ed9094de86523422f6b82bcf6b3ef7b9816
pgfisico <pgfisico.dev@gmail.com>
2020-02-22 23:02:21 -0800

    Verify domain for AWS SES

---
+++ aws_ses.tf
@@ -0,0 +1,17 @@
+resource "aws_ses_domain_identity" "main" {
+  domain = "intimitrons.ca"
+}
+
+resource "aws_route53_record" "aws_ses_verification" {
+  zone_id = data.terraform_remote_state.dns.outputs.zone_id
+  name    = "_amazonses"
+  type    = "TXT"
+  ttl     = 1800
+  records = ["${aws_ses_domain_identity.main.verification_token}"]
+}
+
+resource "aws_ses_domain_identity_verification" "main" {
+  domain = aws_ses_domain_identity.main.domain
+
+  depends_on = aws_route53_record.aws_ses_verification
+}
