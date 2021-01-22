6946c4b9fff6e72a5b1add9a4d39813cdbdb8118
pgfisico <pgfisico.dev@gmail.com>
2021-01-21 21:37:48 -0700

    Decommission StableHost email
    See #18

--- email.tf
+++ email.tf
@@ -12,25 +12,6 @@ resource "aws_route53_record" "email_mx" {
   ]
 }
 
-// mail.intimitrons.ca is currently in use by mail clients
-resource "aws_route53_record" "email_mail" {
-  zone_id = data.terraform_remote_state.dns.outputs.zone_id
-  name    = "mail"
-  type    = "A"
-  ttl     = 3600
-  records = ["198.20.92.28"]
-}
-
-// webmail.intimitrons.ca accesses the web client
-// Likely unused by most on the team but maintained for now to ensure there is always a way to access email
-resource "aws_route53_record" "email_webmail" {
-  zone_id = data.terraform_remote_state.dns.outputs.zone_id
-  name    = "webmail"
-  type    = "A"
-  ttl     = 3600
-  records = ["198.20.92.28"]
-}
-
 resource "aws_route53_record" "email_spf" {
   zone_id = data.terraform_remote_state.dns.outputs.zone_id
   name    = ""
